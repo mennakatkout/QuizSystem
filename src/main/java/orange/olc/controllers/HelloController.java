c14f7f04965605269a1f2c96f6c17bc16325885c
hpkt3764 <melshazly.ext@orange.com>
2017-02-19 17:53:32 +0200

    adding configurations

--- src/main/java/orange/olc/controllers/HelloController.java
+++ src/main/java/orange/olc/controllers/HelloController.java
@@ -9,11 +9,14 @@ package orange.olc.controllers;
  *
  * @author Menna Hisham
  */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping("/")
     public String index() {
