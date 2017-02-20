ba62d21a7a76a15749b883e8c0b490169c453b7b
hpkt3764 <melshazly.ext@orange.com>
2017-02-20 14:56:50 +0200

    spring boot & angular js

---
+++ src/main/java/orange/olc/configurations/WebConfig.java
@@ -0,0 +1,18 @@
+package orange.olc.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Menna Hisham
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
